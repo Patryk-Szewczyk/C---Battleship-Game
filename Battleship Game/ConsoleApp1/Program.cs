4332fee60d62b4cabadc6cea2f4b376b32fa01ad
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2023-11-01 14:26:49 +0100

    Add files via upload

---
+++ Battleship Game/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Battleship Game
+
+using System;
+using OutputProgram;
+using InputWorkProgram;
+
+namespace OutputProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BBBBBBB     BBBB    BBBBBBBB  BBBBBBBB  BB        BBBBBBBB   BBBBBBB  BB    BB  BB  BBBBBBB");
+            Console.WriteLine("BB    BB   BB  BB      BB        BB     BB        BB        BB        BB    BB  BB  BB    BB");
+            Console.WriteLine("BB    BB  BB    BB     BB        BB     BB        BB        BB        BB    BB  BB  BB    BB");
+            Console.WriteLine("BBBBBBBB  BBBBBBBB     BB        BB     BB        BBBBBBBB   BBBBBB   BBBBBBBB  BB  BBBBBBB");
+            Console.WriteLine("BB    BB  BB    BB     BB        BB     BB        BB              BB  BB    BB  BB  BB");
+            Console.WriteLine("BB    BB  BB    BB     BB        BB     BB        BB              BB  BB    BB  BB  BB");
+            Console.WriteLine("BBBBBBB   BB    BB     BB        BB     BBBBBBBB  BBBBBBBB  BBBBBBB   BB    BB  BB  BB");
+            Console.WriteLine("");
+            Console.WriteLine("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+            Console.WriteLine("");
+            Console.WriteLine("Battleship Game is simple game which depend of sunking shipse between players.");
+            Console.WriteLine("To start game you must do a few activites to mainly set ships.");
+            // Następna strona
+        }
+    }
+}
+
+namespace InputWorkProgram
+{
+    public class InitialMenu : Program
+    {
+        static string GameMenu()
+        {
+            return "d4";
+        }
+    }
+}
